64769181c5033d07f187f32ee84b182e38436d16
Laura <jlcunningham2@comcast.net>
2021-05-22 15:30:58 -0600

    added path('', include('entries.urls'), name='entries')

--- journalapp/urls.py
+++ journalapp/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from entries import views as entries_view
 
 urlpatterns = [
+    path('', include('entries.urls'), name='entries'),
     path('admin/', admin.site.urls),
 ]
