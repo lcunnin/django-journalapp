4fa69f6e7f7e8d83d3689965b5b6b678c3b8404d
Laura <jlcunningham2@comcast.net>
2021-05-22 14:02:04 -0600

    added path

--- entries/views.py
+++ entries/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'template.html')
+    return render(request, 'entries/template.html')
