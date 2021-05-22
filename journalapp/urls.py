855d60ffc87fe98dba061526dad65ae9dc4d70eb
Laura <jlcunningham2@comcast.net>
2021-05-22 15:12:56 -0600

    updated html

--- journalapp/urls.py
+++ journalapp/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from entries import views
+from entries import view as entries_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
+    path('', entries_view.index, name="index"),
 ]
