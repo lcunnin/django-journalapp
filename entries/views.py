6fc31d0a4bafb0bd4e5ae0034ac040e0ffa983e9
Laura <jlcunningham2@comcast.net>
2021-05-23 21:22:32 -0600

    created form.py

--- entries/views.py
+++ entries/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Entry
+from .forms import EntryForm
+
 # Create your views here.
 def index(request):
-    entries = Entry.objects.all()
+    entries = Entry.objects.order_by('-date_posted')
 
     context = {'entires' : entries}
+
     return render(request, 'entries/home.html', context)
 
 def add(request):
-    return render(request, 'entries/add.html')
+    form = EntryForm()
+
+    context = {'form' : form}
+    
+    return render(request, 'entries/add.html', context)
