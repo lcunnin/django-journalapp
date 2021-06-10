a86ac5241a4e007df9019b33cebdc87116a0810c
Laura <jlcunningham2@comcast.net>
2021-06-09 22:54:03 -0600

    added crispy forms

--- entries/forms.py
+++ entries/forms.py
@@ -5,10 +5,9 @@ from django.forms import ModelForm
 from .models import Entry
 
 class UserRegistrationForm(UserCreationForm):
-    email=forms.EmailField()
     class Meta:
         model=User
-        fields=['username','email','password1','password2']
+        fields=['username','password1','password2']
 
 class EntryForm(ModelForm):
     class Meta:
