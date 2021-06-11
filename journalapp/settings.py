c83d789bc5141c6c8f26fc814752765b239d7976
Laura <jlcunningham2@comcast.net>
2021-06-10 20:47:58 -0600

    css users

--- journalapp/settings.py
+++ journalapp/settings.py
@@ -125,7 +125,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL='static/'
-CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL='index'
 LOGIN_URL='login'
