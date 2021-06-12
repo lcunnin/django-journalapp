4b040ef251348a2a72cbb8469b36a87647cd5b70
Laura <jlcunningham2@comcast.net>
2021-06-12 14:34:56 -0600

    user data error

--- entries/models.py
+++ entries/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Entry(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="new_entry", null=True)
     title = models.CharField(max_length=100, default='')
     date_posted = models.DateTimeField(auto_now_add = True)
     text = models.TextField(blank = False)
